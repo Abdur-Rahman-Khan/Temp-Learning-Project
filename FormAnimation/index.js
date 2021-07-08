ab8177071c5e928d370733bff01e0c596c73a423
Abdur-Rahman-Khan <71510046+Abdur-Rahman-Khan@users.noreply.github.com>
2021-07-08 19:12:35 +0530

    Login Form

---
+++ FormAnimation/index.js
@@ -0,0 +1,7 @@
+// label=document.querySelectorAll('label');
+// label.forEach((x)=>{
+//     console.log(x.textContent);
+//     y=x.textContent.split('').map((z)=>{return `<span>${z}</apan>`}).join('');
+//     console.log(y);
+//    x.innerHTML=y;
+// })
